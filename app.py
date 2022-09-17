bf927a184e6ac32e3e1af1d0fdb9266f5d58c56a
Elis <67229567+GUNELIS@users.noreply.github.com>
2022-09-17 15:50:42 +0400

    stuff

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, jsonify, request
 import logging
 
-import hash_password
-import post_customer
+import hash_password as hash
+import post_customer as post
 from Customer import Customer
 from DataBase import DataBase
 
 app = Flask(__name__)
 db = DataBase()
-logging.basicConfig(filename='record.log', level=logging.DEBUG)
+# logging.basicConfig(filename='record.log', level=logging.DEBUG)
 
 
 #Test
@@ -17,16 +17,50 @@ def get_name():
     return jsonify(message='Heyyy')
 
 
+
 @app.route("/", methods=["POST"])
-def create_customer():
-    # TODO: error handeling should happend here
-    logging.info('logged in successfully') # logger test
-    password = request.json["password"]
-    password, salt = hash_password.hash_salt_and_pepper(password)
-    customer = Customer(request.json["id"], password, request.json["server"], request.json["actions"], salt)
-    customer = post_customer.post_customer(db, customer)
-    data = customer.dictionary()
-    return jsonify(message='Customer',
+def login():
+    try_id = request.json["id"]
+    try_pswrd = request.json["password"]
+    if post.costumer_id_exists(try_id, db):
+        existing_cust = post.get_customer_from_id(try_id)
+        if post.check_password(existing_cust, try_pswrd,hash ) == False:
+            return jsonify(message='Error - wrong password',
+                   category='Fail',
+                   data=data,
+                   status=200)
+        else:
+            return jsonify(message='Password validated correctly!',
                    category='success',
                    data=data,
                    status=200)
+
+    else:
+        logging.info('logged in successfully') # logger test
+        # if password
+        try_pswrd, salt = hash_password.hash_salt_and_pepper(try_pswrd)
+        customer = Customer(try_id, try_pswrd, request.json["server"], request.json["actions"], salt)
+        customer = post.post_customer(db, customer)
+        data = customer.dictionary()
+        return jsonify(message='Customer',
+                   category='success',
+                   data=data,
+                   status=200)
+
+
+
+
+# @app.route("/", methods=["POST"])
+# def create_customer():
+#     # TODO: error handeling should happend here
+#     logging.info('logged in successfully') # logger test
+#     password = request.json["password"]
+#     # if password
+#     password, salt = hash_password.hash_salt_and_pepper(password)
+#     customer = Customer(request.json["id"], password, request.json["server"], request.json["actions"], salt)
+#     customer = post_customer.post_customer(db, customer)
+#     data = customer.dictionary()
+#     return jsonify(message='Customer',
+#                    category='success',
+#                    data=data,
+#                    status=200)
--- post_customer.py
+++ post_customer.py
@@ -2,20 +2,33 @@
 # from dataBase import dataBase
 
 
+from dataclasses import dataclass
+from sqlite3 import DatabaseError
+from Customer import Customer
+
+from DataBase import DataBase
+
+
 def post_customer(dataBase, customer):
     dataBase.add_customer(customer)
     return customer
 
 
 # check if customer id already exist
-def check_customer_id(customer_id):
-    # for customer in dataBase.get_customer():
-    #     if customer_id == customer['customer_id']:
-    #         return True
+def costumer_id_exists(customer_id, dataBase):
+    for customer in dataBase.get_customers():
+        if customer_id == customer['customer_id']:
+            return True
         return False
 
+def get_customer_from_id(customer_id, dataBase):
+    for customer in dataBase.get_customers():
+        if customer_id == customer['customer_id']:
+            return customer
 
-# TODO
-# check if the password is correct
-def check_password(customer):
+# The password is checked with the given assumption that the id is already verified/ existing.  
+# There is only one password for a given attempted password so we check the passwords one to one 
+def check_password(customer, try_pswrd):
+    if try_pswrd == customer.password:
+        return True
     return False
